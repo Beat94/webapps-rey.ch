3c95d20c01083fbd7e561c5bc7e550dd957b30c3
Beat94 <36741096+Beat94@users.noreply.github.com>
2020-05-11 00:47:20 +0200

    Actualization of routing + Adding timecalculation

--- webpage/scripts/vue-routing.js
+++ webpage/scripts/vue-routing.js
@@ -1,12 +1,15 @@
-Vue.component('app-home', {	template: '<div><p>home - Hello World</p></div>'});
+Vue.component('navigation-index', {template: '<nav><router-link to="/">Home</router-link> <router-link to="/zeitrechner">Zeitrechner</router-link></nav>'});
 
-Vue.component('about', { template: '<div><p>about</p></div>'});
+Vue.component('app-home', {	template: '<div><h1>Home</h1> <p>Willkommen auf der Homepage der Webapps von rey.ch</p></div>'});
+
+Vue.component('timecalc', { template: '<div><form><input type="number" id="hour"> : <input type="number" id="minute"><br><br><button type="button" onclick="timeById()">calculate</button></form><div id="ausgabe"></div></div>'});
+
 
 
 const router = new VueRouter({
 	routes:[
 		{ path: '/', component: { template: '<app-home></app-home>'}},
-		{ path: '/about', component: { template: '<about></about>'}}
+		{ path: '/zeitrechner', component: { template: '<timecalc></timecalc>'}}
 	]
 });
 
@@ -15,5 +18,25 @@ const router = new VueRouter({
 
 var app = new Vue({
 	el: '#app',
-	router
+	router,
+	data: {
+		minute: 0,
+		hour: 0,
+		result: 0,
+		minDec: 0
+	},
+	methods: {
+		calculate: function(){
+			this.result = parseInt(this.hour) + (parseInt(this.minute) / 60)
+		}
+	}
 });
+
+function timeById(){
+	stunde = document.querySelector("#hour").value;
+	minuten = document.querySelector("#minute").value;
+	console.log("Stunde: " + stunde);
+	console.log("Minute: " + minuten);
+	resultat = parseInt(stunde) + (parseInt(minuten) / 60);
+	document.getElementById("ausgabe").innerHTML = resultat;
+}
