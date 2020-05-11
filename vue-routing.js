a2274facda808c24d5e66cb330aad1b867c8ad63
Beat94 <36741096+Beat94@users.noreply.github.com>
2020-05-11 21:46:51 +0200

    update vue-routing.js
    deleted some unused stuff

---
+++ vue-routing.js
@@ -0,0 +1,29 @@
+Vue.component('navigation-index', {template: '<nav><router-link to="/">Home</router-link> <router-link to="/zeitrechner">Zeitrechner</router-link></nav>'});
+
+Vue.component('app-home', {	template: '<div><h1>Home</h1> <p>Willkommen auf der Homepage der Webapps von rey.ch</p></div>'});
+
+Vue.component('timecalc', { template: '<div><h1>Zeitrechner</h1><form><input type="number" id="hour"> : <input type="number" id="minute"><br><br><button type="button" onclick="timeById()">calculate</button></form><div id="ausgabe"></div></div>'});
+
+
+
+const router = new VueRouter({
+	routes:[
+		{ path: '/', component: { template: '<app-home></app-home>'}},
+		{ path: '/zeitrechner', component: { template: '<timecalc></timecalc>'}}
+	]
+});
+
+
+
+
+var app = new Vue({
+	el: '#app',
+	router
+});
+
+function timeById(){
+	stunde = document.querySelector("#hour").value;
+	minuten = document.querySelector("#minute").value;
+	resultat = parseInt(stunde) + (parseInt(minuten) / 60);
+	document.getElementById("ausgabe").innerHTML = resultat;
+}
