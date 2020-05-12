aa26476b37ac470718ed4082e59721523e515dac
Beat94 <36741096+Beat94@users.noreply.github.com>
2020-05-12 08:50:39 +0200

    Update vue-routing.js

--- webpage/scripts/vue-routing.js
+++ webpage/scripts/vue-routing.js
@@ -1,7 +1,6 @@
 Vue.component('navigation-index', {template: '<nav><router-link to="/">Home</router-link> <router-link to="/zeitrechner">Zeitrechner</router-link></nav>'});
 
-Vue.component('app-home', {	template: '<div><h1>Home</h1> <p>Willkommen auf der Homepage der Webapps von rey.ch</p></div>'});
-
+Vue.component('app-home', { template: '<div><h1>Home</h1> <p>Willkommen auf der Homepage der Webapps von rey.ch</p></div>'});
 Vue.component('timecalc', { template: '<div><h1>Zeitrechner</h1><form><input type="number" id="hour"> : <input type="number" id="minute"><br><br><button type="button" onclick="timeById()">calculate</button></form><div id="ausgabe"></div></div>'});
 
 
