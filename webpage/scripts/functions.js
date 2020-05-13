4a79bfa2e56a5cdb60d0d8f17493311032517170
Beat94 <36741096+Beat94@users.noreply.github.com>
2020-05-13 23:49:48 +0200

    Implementation midnight formula
    Added a new route in vue route
    
    Added new method in functions
    
    Actualized navigation

--- webpage/scripts/functions.js
+++ webpage/scripts/functions.js
@@ -4,6 +4,31 @@ function toHours(hour, minute){
 	return hourInt + (minuteInt / 60);
 }
 
+function midnight(){
+	varA = document.querySelector("#variableA").value;
+	varB = document.querySelector("#variableB").value;
+	varC = document.querySelector("#variableC").value;
+	
+	varAInt = parseInt(varA);
+	varBInt = parseInt(varB);
+	varCInt = parseInt(varC);
+	
+	result1 = ((varBInt * (-1)) + Math.pow(Math.pow(varBInt, 2) - 4 * varAInt * varCInt, 0.5)) / (2 * varAInt);
+	result2 = ((varBInt * (-1)) - Math.pow(Math.pow(varBInt, 2) - 4 * varAInt * varCInt, 0.5)) / (2 * varAInt);
+	
+	
+	//output
+	//console.log(result1);
+	//console.log(result2);
+	document.getElementById("output").innerHTML = "X1: \t" + result1 + "<br>";
+	document.getElementById("output").innerHTML += "X2: \t" + result2;
+	
+}
+
+
+
+
+
 function timeById(){
 	stunde = document.querySelector("#hour").value;
 	minuten = document.querySelector("#minute").value;
