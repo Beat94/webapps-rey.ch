c73b6e4964a5b0ccf0df0a5c2d104195ba781c7b
Beat94 <36741096+Beat94@users.noreply.github.com>
2020-05-13 19:11:37 +0200

    GUI optimization
    Optimized titles and spacing of the border.

--- webpage/scripts/vue-routing.js
+++ webpage/scripts/vue-routing.js
@@ -1,7 +1,7 @@
 Vue.component('navigation-index', {template: '<nav><router-link to="/">Home</router-link> <router-link to="/zeitrechner">Zeitrechner</router-link></nav>'});
 
-Vue.component('app-home', { template: '<div><h1>Home</h1> <p>Willkommen auf der Homepage der Webapps von rey.ch</p></div>'});
-Vue.component('timecalc', { template: '<div><h1>Zeitrechner</h1><form><input type="number" id="hour"> : <input type="number" id="minute"><br><br><button type="button" onclick="timeById()">calculate</button></form><div id="ausgabe"></div></div>'});
+Vue.component('app-home', { template: '<div><h2>Home</h2> <p>Willkommen auf der Homepage der Webapps von rey.ch</p></div>'});
+Vue.component('timecalc', { template: '<div><h2>Zeitrechner</h2><form><input type="number" id="hour"> : <input type="number" id="minute"><br><br><button type="button" onclick="timeById()">calculate</button></form><div id="ausgabe"></div></div>'});
 
 
 
