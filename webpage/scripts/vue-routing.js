c56f1de4911c86e819d9309fe22051ecc6644c7e
Beat94 <36741096+Beat94@users.noreply.github.com>
2020-05-15 18:55:04 +0200

    Calculation on Enter
    Implemented the dark calculation-button
    
    corrected the type of timecalc

--- webpage/scripts/vue-routing.js
+++ webpage/scripts/vue-routing.js
@@ -1,8 +1,8 @@
 Vue.component('navigation-index', {template: '<nav><router-link to="/">Home</router-link> | <router-link to="/zeitrechner">Zeitrechner</router-link> | <router-link to="/midnightformula">Mitternachtsformel</router-link></nav>'});
 
 Vue.component('app-home', { template: '<div><h2>Home</h2> <p>Willkommen auf der Homepage der Webapps von rey.ch</p></div>'});
-Vue.component('timecalc', { template: '<div><h2>Zeitrechner</h2><form><input type="text" id="hour" style="width:60px;"> : <input type="text" id="minute" style="width:60px;"><br><br><button type="button" onclick="timeById()" class="btn btn-dark">calculate</button></form><div id="ausgabe"></div></div>'});
-Vue.component('midnight', { template: '<div><h2>Mitternachtsformel</h2><form><table><tr><td>A:</td><td><input type="number" id="variableA"></td></tr><tr><td>B:</td><td><input type="number" id="variableB"></td></tr><tr><td>C:</td><td><input type="number" id="variableC"></td></tr></table><br>	<button onclick="midnight()">Calculate</button><br><br>L&oumlsung:<br><div id="output"></div></form></div>'});
+Vue.component('timecalc', { template: '<div><h2>Zeitrechner</h2><form><input type="text" id="hour" style="width:60px;"> : <input type="text" id="minute" style="width:60px;"><br><br><button id="btnTimeCalc" class="btn btn-dark" onclick="timeById()">Calculate</button></form><div id="ausgabe"></div></div>'});
+Vue.component('midnight', { template: '<div><h2>Mitternachtsformel</h2><form><table><tr><td>A:</td><td><input type="number" id="variableA"></td></tr><tr><td>B:</td><td><input type="number" id="variableB"></td></tr><tr><td>C:</td><td><input type="number" id="variableC"></td></tr></table><br>	<button class="btn btn-dark" id="btnCalc" onclick="midnight()">Calculate</button><br><br>L&oumlsung:<br><div id="output"></div></form></div>'});
 
 
 
